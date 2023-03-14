ece134d67dd8e153e3d057ab1c6d48e5fbc7926e
iamMAHAM <abdulkabore@gmail.com>
2023-03-14 08:33:16 +0000

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,8 @@
     <div class="inner-footer">
       <div class="footer-items">
         <h1>Immo Soluce</h1>
-        <p>
-          La solution aux problèmes immobiliers en Côte d'Ivoire
-        </p>
-        <img src="@/assets/logo.png">
+        <p>La solution aux problèmes immobiliers en Côte d'Ivoire</p>
+        <img src="@/assets/logo.png" />
       </div>
       <div class="footer-items">
         <h3>Nos Agences</h3>
@@ -27,9 +25,18 @@
         <h3>Contactez Nous</h3>
         <div class="border1"></div>
         <ul>
-          <li><i class="material-symbols-outlined">location_on</i>Abidjan, Angré 7ème tranche</li>
+          <li>
+            <i class="material-symbols-outlined">location_on</i>Abidjan, Angré
+            7ème tranche
+          </li>
           <li><i class="material-symbols-outlined">call</i>+2250000000000</li>
-          <li><i i class="material-symbols-outlined">mail</i>immosoluce@gmail.com</li>
+          <li>
+            <i
+              i
+              class="material-symbols-outlined"
+              >mail</i
+            >immosoluce@gmail.com
+          </li>
         </ul>
         <div class="social-media">
           <a href="#"><i class="fab fa-instagram"></i></a>
@@ -39,54 +46,54 @@
       </div>
     </div>
     <div class="footer-bottom">
-      Copyright &copy; Immo Soluce Team 2022.
+      Copyright &copy; Immo Soluce Team 2023. All right reserved
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
-}
+  name: 'Footer',
+};
 </script>
 
 <style scoped>
 /* FOOTER */
 .footer {
-	width: 100%;
-	background: var(--navcolor);
-	display: block;
- }
+  width: 100%;
+  background: var(--navcolor);
+  display: block;
+}
 
- .inner-footer {
-	 width: 95%;
-	 margin: auto;
-	 padding: 30px 10px;
-	 display: flex;
-	 flex-wrap: wrap;
-	 box-sizing: border-box;
-	 justify-content: center;
- }
+.inner-footer {
+  width: 95%;
+  margin: auto;
+  padding: 30px 10px;
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  justify-content: center;
+}
 
 .footer-items {
-	width: 25%;
-	padding: 10px 20px;
-	box-sizing: border-box;
-	color: #fff;
+  width: 25%;
+  padding: 10px 20px;
+  box-sizing: border-box;
+  color: #fff;
 }
 
 .footer-items p {
-	font-size: 16px;
-	text-align: justify;
-	line-height: 25px;
-	color: #fff;
+  font-size: 16px;
+  text-align: justify;
+  line-height: 25px;
+  color: #fff;
 }
 
 .footer-items h1 {
-	color: #fff;
+  color: #fff;
 }
 
-.footer img{
+.footer img {
   height: 10rem;
   width: 10rem;
   margin: 0 auto;
@@ -94,87 +101,87 @@ export default {
 }
 
 .border1 {
-	height: 3px;
-	width: 40px;
-	background: var(--hovercolor);
-	color: var(--hovercolor);
-	background-color: var(--hovercolor);
-	border: 0px;
+  height: 3px;
+  width: 40px;
+  background: var(--hovercolor);
+  color: var(--hovercolor);
+  background-color: var(--hovercolor);
+  border: 0px;
 }
 
 ul {
-	list-style: none;
-	color: #fff;
-	font-size: 15px;
-	letter-spacing: 0.5px;	
- }
+  list-style: none;
+  color: #fff;
+  font-size: 15px;
+  letter-spacing: 0.5px;
+}
 
 ul a {
-	text-decoration: none;
-	outline: none;
-	color: #fff;
-	transition: 0.3s;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  transition: 0.3s;
 }
 
 ul a:hover {
-	color: var(--hovercolor);
+  color: var(--hovercolor);
 }
 
 ul li {
-	margin: 10px 0;
-	height: 25px;
+  margin: 10px 0;
+  height: 25px;
 }
 
 li i {
-	margin-right: 20px;
+  margin-right: 20px;
 }
 
 .social-media {
-	width: 100%;
-	color: #fff;
-	text-align: center;
-	font-size: 20px;
+  width: 100%;
+  color: #fff;
+  text-align: center;
+  font-size: 20px;
 }
 
 .social-media a {
-	text-decoration: none;
+  text-decoration: none;
 }
 
 .social-media i {
-	height: 25px;
-	width: 25px;
-	margin: 20px 10px;
-	padding: 4px;
-	color: #fff;
-	transition: 0.5s;
+  height: 25px;
+  width: 25px;
+  margin: 20px 10px;
+  padding: 4px;
+  color: #fff;
+  transition: 0.5s;
 }
 
 .social-media i:hover {
-	transform: scale(1.5);
+  transform: scale(1.5);
 }
 
 .footer-bottom {
-	padding: 10px;
-	background: var(--navcolor);
-	color: #fff;
-	font-size: 12px;
-	text-align: center;
+  padding: 10px;
+  background: var(--navcolor);
+  color: #fff;
+  font-size: 12px;
+  text-align: center;
 }
 
 /* for tablet mode view */
 
 @media screen and (max-width: 1275px) {
-	.footer-items {
-		width: 50%;
-	}
+  .footer-items {
+    width: 50%;
+  }
 }
 
 /* for mobile screen view */
 
 @media screen and (max-width: 660px) {
-	.footer-items {
-		width: 100%;
-	}
+  .footer-items {
+    width: 100%;
+  }
 }
 
 /* you can toggle the media screen view accordingly by changing the (max-width: px) to your convenience */
--- src/components/partials/user/Pagination.vue
+++ src/components/partials/user/Pagination.vue
@@ -3,46 +3,51 @@
     :total-items="items.length"
     :items-per-page="20"
     :max-pages-shown="5"
-    :current-page="1"
+    v-model="currentPage"
     :on-click="onClickHandler"
   />
 </template>
 
 <script>
-export default{
+export default {
   name: 'Pagination',
   props: ['items'],
-  methods:{
-    onClickHandler(page){
-      this.$emit('changePage', page)
-    }
-  }
-}
+  data() {
+    return {
+      currentPage: 1,
+    };
+  },
+  methods: {
+    onClickHandler(page) {
+      this.$emit('changePage', page);
+    },
+  },
+};
 </script>
 
 <style>
-  .pagination-container {
-    display: flex;
-    column-gap: 10px;
-  }
-  .paginate-buttons {
-    height: 40px;
-    width: 40px;
-    border-radius: 20px;
-    cursor: pointer;
-    background-color: rgb(242, 242, 242);
-    border: 1px solid rgb(217, 217, 217);
-    color: black;
-  }
-  .paginate-buttons:hover {
-    background-color: #d8d8d8;
-  }
-  .active-page {
-    background-color: var(--greenfun);
-    border: 1px solid var(--white);
-    color: white;
-  }
-  .active-page:hover {
-    background-color: var(--greenfun);
-  }
+.pagination-container {
+  display: flex;
+  column-gap: 10px;
+}
+.paginate-buttons {
+  height: 40px;
+  width: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: rgb(242, 242, 242);
+  border: 1px solid rgb(217, 217, 217);
+  color: black;
+}
+.paginate-buttons:hover {
+  background-color: #d8d8d8;
+}
+.active-page {
+  background-color: var(--greenfun);
+  border: 1px solid var(--white);
+  color: white;
+}
+.active-page:hover {
+  background-color: var(--greenfun);
+}
 </style>
